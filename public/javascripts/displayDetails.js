8b6940ea420f176b3241fa8c99054e13f5ef0983
raveoli <raveena.hegde@gmail.com>
2018-04-30 17:34:06 -0500

    Added chart

--- public/javascripts/displayDetails.js
+++ public/javascripts/displayDetails.js
@@ -91,108 +91,7 @@ app.controller("infoCtrl", ['$scope','$http','$filter',function($scope, $http, $
                 }
             }
             //console.log(yearArr_base.length);
-            switch(baseValue){
-                case 'United States':break;
-                case 'Texas': break;
-                case 'Dallas County, Texas': break;
-                case 'Dallas city, Texas': break;
-                case 'Dallas-Fort Worth-Arlington, TX Metro Area': break;
-                case 'ZCTA5 75001': break;
-                case 'ZCTA5 75006': break;
-                case 'ZCTA5 75007': break;
-                case 'ZCTA5 75019': break;
-                case 'ZCTA5 75038': break;
-                case 'ZCTA5 75039': break;
-                case 'ZCTA5 75040': break;
-                case 'ZCTA5 75041': break;
-                case 'ZCTA5 75042': break;
-                case 'ZCTA5 75043': break;
-                case 'ZCTA5 75044': break;
-                case 'ZCTA5 75048': break;
-                case 'ZCTA5 75050': break;
-                case 'ZCTA5 75051': break;
-                case 'ZCTA5 75052': break;
-                case 'ZCTA5 75054': break;
-                case 'ZCTA5 75060': break;
-                case 'ZCTA5 75061': break;
-                case 'ZCTA5 75062': break;
-                case 'ZCTA5 75063': break;
-                case 'ZCTA5 75067': break;
-                case 'ZCTA5 75080': break;
-                case 'ZCTA5 75081': break;
-                case 'ZCTA5 75082': break;
-                case 'ZCTA5 75088': break;
-                case 'ZCTA5 75089': break;
-                case 'ZCTA5 75098': break;
-                case 'ZCTA5 75104': break;
-                case 'ZCTA5 75115': break;
-                case 'ZCTA5 75116': break;
-                case 'ZCTA5 75125': break;
-                case 'ZCTA5 75134': break;
-                case 'ZCTA5 75137': break;
-                case 'ZCTA5 75141': break;
-                case 'ZCTA5 75146': break;
-                case 'ZCTA5 75149': break;
-                case 'ZCTA5 75150': break;
-                case 'ZCTA5 75154': break;
-                case 'ZCTA5 75159': break;
-                case 'ZCTA5 75172': break;
-                case 'ZCTA5 75180': break;
-                case 'ZCTA5 75181': break;
-                case 'ZCTA5 75182': break;
-                case 'ZCTA5 75201': break;
-                case 'ZCTA5 75202': break;
-                case 'ZCTA5 75203': break;
-                case 'ZCTA5 75204': break;
-                case 'ZCTA5 75205': break;
-                case 'ZCTA5 75206': break;
-                case 'ZCTA5 75207': break;
-                case 'ZCTA5 75208': break;
-                case 'ZCTA5 75209': break;
-                case 'ZCTA5 75210': break;
-                case 'ZCTA5 75211': break;
-                case 'ZCTA5 75212': break;
-                case 'ZCTA5 75214': break;
-                case 'ZCTA5 75215': break;
-                case 'ZCTA5 75216': break;
-                case 'ZCTA5 75217': break;
-                case 'ZCTA5 75218': break;
-                case 'ZCTA5 75219': break;
-                case 'ZCTA5 75220': break;
-                case 'ZCTA5 75223': break;
-                case 'ZCTA5 75224': break;
-                case 'ZCTA5 75225': break;
-                case 'ZCTA5 75226': break;
-                case 'ZCTA5 75227': break;
-                case 'ZCTA5 75228': break;
-                case 'ZCTA5 75229': break;
-                case 'ZCTA5 75230': break;
-                case 'ZCTA5 75231': break;
-                case 'ZCTA5 75232': break;
-                case 'ZCTA5 75233': break;
-                case 'ZCTA5 75234': break;
-                case 'ZCTA5 75235': break;
-                case 'ZCTA5 75236': break;
-                case 'ZCTA5 75237': break;
-                case 'ZCTA5 75238': break;
-                case 'ZCTA5 75240': break;
-                case 'ZCTA5 75241': break;
-                case 'ZCTA5 75243': break;
-                case 'ZCTA5 75244': break;
-                case 'ZCTA5 75246': break;
-                case 'ZCTA5 75247': break;
-                case 'ZCTA5 75248': break;
-                case 'ZCTA5 75249': break;
-                case 'ZCTA5 75251': break;
-                case 'ZCTA5 75252': break;
-                case 'ZCTA5 75253': break;
-                case 'ZCTA5 75254': break;
-                case 'ZCTA5 75270': break;
-                case 'ZCTA5 75287': break;
-                case 'ZCTA5 75390': break;
-                case 'ZCTA5 76051': break;
-                case 'ZCTA5 76155': break;
-            }
+
         });
         $('#compare').on('change',function () {
             cmpValue = $(this).val();
@@ -211,39 +110,54 @@ app.controller("infoCtrl", ['$scope','$http','$filter',function($scope, $http, $
         });
         $scope.displayYear=function () {
             year=match(yearArr_base,yearArr_cmp);
-            console.log("Year:");
+            var num=Math.min(base_povrate.length,cmp_povrate.length);
+            var base_povrate1=arrSplit(base_povrate,num);
+            var cmp_povrate1=arrSplit(cmp_povrate,num);
+            //console.log("Year:");
             for(var i=0;i<year.length;i++){
-                console.log(year[i]);
+                //console.log(year[i]);
+                year[i].basepov=base_povrate1[i];
+                year[i].cmppov=cmp_povrate1[i];
             }
-            $scope.yearList=year;
-            $scope.tableYear=year;
+            $scope.povRate={
+                povrate:year
+            };
+
             $scope.base=baseValue;
             $scope.cmp=cmpValue;
 
-            var num=Math.min(base_povrate.length,cmp_povrate.length);
-            base_povrate=arrSplit(base_povrate,num);
-            cmp_povrate=arrSplit(cmp_povrate,num);
-           /*console.log("Base:");
-             for(var i=0;i<base_povrate.length;i++){
-             console.log(base_povrate[i]);
-             }
-             console.log("Cmpareo:");
-             for(var i=0;i<base_povrate.length;i++){
-             console.log(cmp_povrate[i]);
-             }*/
-            $scope.povList={
-                basepov:base_povrate
-            };
-            $scope.povList1= {
-                cmppov:cmp_povrate
-            };
-        }
-        $scope.doIfChecked=function(item){
-            //console.log(item);
-            //write logic to hide respective columns from table
+            google.charts.load('current', {'packages':['line']});
+            google.charts.setOnLoadCallback(drawChart);
+            function drawChart() {
+                var dataArray=[];
+                for(var i=0;i<year.length;i++){
+                    dataArray.push([year[i].year,year[i].basepov,year[i].cmppov]);
+                }
+                var data= new google.visualization.DataTable();
+                data.addColumn('number','Year');
+                data.addColumn('number',baseValue);
+                data.addColumn('number',cmpValue);
 
+                data.addRows(dataArray);
+                var options = {
+                    chart: {
+                        title: 'Poverty Rate of two countries'
+                    },
+                    hAxis: {format:'####'},
+                    vAxis: {format:'0.0'}
+                };
+                var formatter1 = new google.visualization.NumberFormat({pattern:'####'});
+                formatter1.format(data, 0);
+                var formatter2 = new google.visualization.NumberFormat({pattern:'0.0'});
+                formatter2.format(data, 1);
+
+                var chart =new google.charts.Line(document.getElementById('curve_chart'));
+
+                chart.draw(data, google.charts.Line.convertOptions(options));
+            }
         }
-       $scope.$watch("yearList", function(n,o){
+
+       /*$scope.$watch("yearList", function(n,o){
            //identifies which checkbox is false
            console.log("n:");
            console.log(n);
@@ -255,8 +169,110 @@ app.controller("infoCtrl", ['$scope','$http','$filter',function($scope, $http, $
            console.log(falses[0]);
             console.log(falses.length);
             //write logic to hide respective columns from table
-        },true);
+        },true);*/
 
 
     });
 }]);
+/*switch(baseValue){
+    case 'United States':break;
+    case 'Texas': break;
+    case 'Dallas County, Texas': break;
+    case 'Dallas city, Texas': break;
+    case 'Dallas-Fort Worth-Arlington, TX Metro Area': break;
+    case 'ZCTA5 75001': break;
+    case 'ZCTA5 75006': break;
+    case 'ZCTA5 75007': break;
+    case 'ZCTA5 75019': break;
+    case 'ZCTA5 75038': break;
+    case 'ZCTA5 75039': break;
+    case 'ZCTA5 75040': break;
+    case 'ZCTA5 75041': break;
+    case 'ZCTA5 75042': break;
+    case 'ZCTA5 75043': break;
+    case 'ZCTA5 75044': break;
+    case 'ZCTA5 75048': break;
+    case 'ZCTA5 75050': break;
+    case 'ZCTA5 75051': break;
+    case 'ZCTA5 75052': break;
+    case 'ZCTA5 75054': break;
+    case 'ZCTA5 75060': break;
+    case 'ZCTA5 75061': break;
+    case 'ZCTA5 75062': break;
+    case 'ZCTA5 75063': break;
+    case 'ZCTA5 75067': break;
+    case 'ZCTA5 75080': break;
+    case 'ZCTA5 75081': break;
+    case 'ZCTA5 75082': break;
+    case 'ZCTA5 75088': break;
+    case 'ZCTA5 75089': break;
+    case 'ZCTA5 75098': break;
+    case 'ZCTA5 75104': break;
+    case 'ZCTA5 75115': break;
+    case 'ZCTA5 75116': break;
+    case 'ZCTA5 75125': break;
+    case 'ZCTA5 75134': break;
+    case 'ZCTA5 75137': break;
+    case 'ZCTA5 75141': break;
+    case 'ZCTA5 75146': break;
+    case 'ZCTA5 75149': break;
+    case 'ZCTA5 75150': break;
+    case 'ZCTA5 75154': break;
+    case 'ZCTA5 75159': break;
+    case 'ZCTA5 75172': break;
+    case 'ZCTA5 75180': break;
+    case 'ZCTA5 75181': break;
+    case 'ZCTA5 75182': break;
+    case 'ZCTA5 75201': break;
+    case 'ZCTA5 75202': break;
+    case 'ZCTA5 75203': break;
+    case 'ZCTA5 75204': break;
+    case 'ZCTA5 75205': break;
+    case 'ZCTA5 75206': break;
+    case 'ZCTA5 75207': break;
+    case 'ZCTA5 75208': break;
+    case 'ZCTA5 75209': break;
+    case 'ZCTA5 75210': break;
+    case 'ZCTA5 75211': break;
+    case 'ZCTA5 75212': break;
+    case 'ZCTA5 75214': break;
+    case 'ZCTA5 75215': break;
+    case 'ZCTA5 75216': break;
+    case 'ZCTA5 75217': break;
+    case 'ZCTA5 75218': break;
+    case 'ZCTA5 75219': break;
+    case 'ZCTA5 75220': break;
+    case 'ZCTA5 75223': break;
+    case 'ZCTA5 75224': break;
+    case 'ZCTA5 75225': break;
+    case 'ZCTA5 75226': break;
+    case 'ZCTA5 75227': break;
+    case 'ZCTA5 75228': break;
+    case 'ZCTA5 75229': break;
+    case 'ZCTA5 75230': break;
+    case 'ZCTA5 75231': break;
+    case 'ZCTA5 75232': break;
+    case 'ZCTA5 75233': break;
+    case 'ZCTA5 75234': break;
+    case 'ZCTA5 75235': break;
+    case 'ZCTA5 75236': break;
+    case 'ZCTA5 75237': break;
+    case 'ZCTA5 75238': break;
+    case 'ZCTA5 75240': break;
+    case 'ZCTA5 75241': break;
+    case 'ZCTA5 75243': break;
+    case 'ZCTA5 75244': break;
+    case 'ZCTA5 75246': break;
+    case 'ZCTA5 75247': break;
+    case 'ZCTA5 75248': break;
+    case 'ZCTA5 75249': break;
+    case 'ZCTA5 75251': break;
+    case 'ZCTA5 75252': break;
+    case 'ZCTA5 75253': break;
+    case 'ZCTA5 75254': break;
+    case 'ZCTA5 75270': break;
+    case 'ZCTA5 75287': break;
+    case 'ZCTA5 75390': break;
+    case 'ZCTA5 76051': break;
+    case 'ZCTA5 76155': break;
+}*/
